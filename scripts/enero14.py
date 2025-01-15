7b96a0409e6b97060bc76211be07ea8f3afc9c17
angelumoca21 <angelmoca21@gmail.com>
2025-01-14 20:00:07 -0600

    14enero

---
+++ scripts/enero14.py
@@ -0,0 +1,10 @@
+"""Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+
+nombre = input("Cual es tu nombre?")
+print("Hola " + nombre)#concatenar
+print(f"Hola {nombre}")
+
+resultado=((3+2)/(2*5))**2
+print(resultado)
+print("Angel"*5)"""
+

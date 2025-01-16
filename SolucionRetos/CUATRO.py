7d460d4b483563e7c952ce2f26935235b5542c96
angelumoca21 <angelmoca21@gmail.com>
2025-01-15 19:54:52 -0600

    enero15

--- SolucionRetos/CUATRO.py
+++ SolucionRetos/CUATRO.py
@@ -9,10 +9,15 @@ Si el color es diferente a verde el usuario gana 10 puntos.
 Si el color ingresado es verde el usuario gane 5 puntos.
 Si el color ingresado es amarillo el usuario gana 10 puntos.
 Si el color ingresado es rojo el usuario gana 15 puntos.
-
-**Si se ingresa un valor inválido terminar programa y mostrar un mensaje de error.**
 """
 
+color = input("Ingresa el color de tu alien:")
+if color == "verde":
+    print("Ganaste 5 puntos")
+elif color == "amarillo":
+    print("Ganaste 10 puntos")
+elif color == "rojo":
+    print("Ganaste 15 puntos")
 """
 Etapas de la vida
 Escribe un código para determinar la etapa de vida de una persona, a partir de la edad ingresada por teclado:
@@ -23,3 +28,20 @@ Si la edad es mínimo 13 y menor a 20, imprime: "Es un adolescente".
 Si la edad va desde 20 y es menor a 65, imprime: "Es un adulto".
 Si la edad es mayor o igual 65, imprime: "Es un adulto mayor".
 """
+
+edad = int(input("Cuantos años tienes?"))
+
+if edad<2 and edad>0:
+    print("Bebé")
+elif edad<4 and edad>=2:
+    print("Niño pequeño.")
+elif edad<13 and edad>=4:
+    print("Niño.")
+elif edad<=19 and edad>12:
+    print("Adolescente")
+elif edad<=64 and edad>19:
+    print("Adulto")
+elif edad>=65:
+    print("Adulto mayor")
+else:
+    print("Edad invalida")
---
+++ scripts/enero15.py
@@ -0,0 +1,34 @@
+#if simple (True)
+
+edad = int(input("Cuantos años tienes?"))
+
+if edad>=18:
+    print("Eres mayor de edad.")
+
+#if-else
+
+edad = int(input("Cuantos años tienes?"))
+
+if edad>=18:
+    print("Eres mayor de edad.")
+else:
+    print("No eres mayor de edad.")
+
+#if-elif-else
+
+edad = int(input("Cuantos años tienes?"))
+
+if edad<2 and edad>0:
+    print("Bebé")
+elif edad<4 and edad>=2:
+    print("Niño pequeño.")
+elif edad<13 and edad>=4:
+    print("Niño.")
+elif edad<=19 and edad>12:
+    print("Adolescente")
+elif edad<=64 and edad>19:
+    print("Adulto")
+elif edad>=65:
+    print("Adulto mayor")
+else:
+    print("Edad invalida")

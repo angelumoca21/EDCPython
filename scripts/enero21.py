1336ec0195ad09571788b602e3392ebf538f32c8
angelumoca21 <angelmoca21@gmail.com>
2025-01-21 20:03:43 -0600

    21enero

---
+++ scripts/enero21.py
@@ -0,0 +1,45 @@
+tupla = ("Angel",25,False,2.9,[1,"a",2,3,"b"])
+#print(type(lista))
+#print(type(tupla))
+'''print(lista)
+lista.insert(2,"Uriel")
+lista[0]=76
+#lista.pop()
+lista.remove(25)
+lista.remove(25)
+lista.reverse()
+print(lista)
+print(lista.count(25))
+print(lista.index(2.9))'''
+'''print(len(tupla)) #cantidad de elementos que contiene una lista o tupla
+
+print(tupla[::])
+
+print(type(lista[-2]))
+
+print(lista[-2][-2])
+
+a = 5
+b = a
+b = b+1
+print(a)
+print(b)
+lista = ["Angel",25,False,2.9,[1,"a",2,3,"b"],25]
+lista2=lista.copy()
+lista2.append("Nuevo Elemento")
+print(lista)
+print(lista2)
+string="AniTa LaVA La tINA"
+string2=string.lower()
+print(string2)
+print(string.replace("a","z"))'''
+
+alumnos = {
+    "Angel":[28,"Lago q #123",5555555555],
+    "Juan":"tyuioy",
+    "Pedro":5
+}
+
+print(alumnos["Pedro"])
+print(alumnos.get("Angel"))
+print(alumnos.items())
